01b917337a87ebe3b518b6fc69df7966b9e3c276
Kilsup Lee <gislee@dgu.ac.kr>
2025-03-31 15:38:28 +0900

    Modify main.py at gdhong

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Kilsup Lee")
+print('hello world')  
+name='gdhong'  
+print(f'I\'m GD Hong!')  
+programming_language='Python'  
+print('Most used language is {0}'.format(programming_language))
